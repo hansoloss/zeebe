222979b1122c24dbae13ec363fafa626cdf503ba
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-02-21 15:25:04 +0100

    chore(logstreams): simplify log storage abstraction
    - documents intent of log storage abstraction
    - simplifies log storage reader abstraction down, removing the random
      access use case in favour of a simple iterative access, with a single
      seek method.
    - prepares the storage implementation to be extracted back into the
      broker

--- logstreams/src/test/java/io/zeebe/logstreams/impl/log/LogStreamReaderTest.java
+++ logstreams/src/test/java/io/zeebe/logstreams/impl/log/LogStreamReaderTest.java
@@ -9,6 +9,7 @@ package io.zeebe.logstreams.impl.log;
 
 import static io.zeebe.util.StringUtil.getBytes;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import io.zeebe.logstreams.log.LogStreamReader;
 import io.zeebe.logstreams.log.LoggedEvent;
@@ -29,7 +30,6 @@ import org.junit.rules.TemporaryFolder;
 public final class LogStreamReaderTest {
   private static final UnsafeBuffer EVENT_VALUE = new UnsafeBuffer(getBytes("test"));
   private static final int LOG_SEGMENT_SIZE = (int) ByteValue.ofMegabytes(4);
-  private static final UnsafeBuffer BIG_EVENT_VALUE = new UnsafeBuffer(new byte[64 * 1024]);
 
   @Rule public final ExpectedException expectedException = ExpectedException.none();
 
@@ -58,17 +58,13 @@ public final class LogStreamReaderTest {
   }
 
   @Test
-  public void shouldThrowExceptionIfReaderClosed() {
+  public void shouldNotHaveNextIfReaderIsClosed() {
     // given
     final LogStreamReader reader = logStreamRule.getLogStreamReader();
     reader.close();
 
-    // expect
-    expectedException.expectMessage(LogStreamReaderImpl.ERROR_CLOSED);
-    expectedException.expect(IllegalStateException.class);
-
-    // when
-    reader.hasNext();
+    // when - then
+    assertThat(reader.hasNext()).isFalse();
   }
 
   @Test
@@ -77,13 +73,8 @@ public final class LogStreamReaderTest {
     final LogStreamReader reader = logStreamRule.getLogStreamReader();
     reader.close();
 
-    // expect
-    expectedException.expectMessage(LogStreamReaderImpl.ERROR_CLOSED);
-    expectedException.expect(IllegalStateException.class);
-
-    // when
-    // then
-    reader.next();
+    // when - then
+    assertThatCode(reader::next).isInstanceOf(NoSuchElementException.class);
   }
 
   @Test
@@ -97,7 +88,7 @@ public final class LogStreamReaderTest {
     final long position = writer.writeEvent(w -> w.key(eventKey).value(EVENT_VALUE));
 
     // then
-    assertThat(reader.hasNext()).isEqualTo(true);
+    assertThat(reader.hasNext()).isTrue();
     final LoggedEvent next = reader.next();
     assertThat(next.getKey()).isEqualTo(eventKey);
     assertThat(next.getPosition()).isEqualTo(position);
@@ -106,14 +97,9 @@ public final class LogStreamReaderTest {
 
   @Test
   public void shouldThrowNoSuchElementExceptionOnNextCall() {
-    // expect
-    expectedException.expectMessage(
-        "Api protocol violation: No next log entry available; You need to probe with hasNext() first.");
-    expectedException.expect(NoSuchElementException.class);
-
-    // when
+    // given an empty log
     // then
-    reader.next();
+    assertThatCode(reader::next).isInstanceOf(NoSuchElementException.class);
   }
 
   @Test
@@ -200,82 +186,16 @@ public final class LogStreamReaderTest {
   }
 
   @Test
-  public void shouldIncreaseBufferAndSeekToLastEventIfSmallAndBigDoesNotFitTogether() {
-    // given
-    final int eventCount = 3;
-    final byte[] bytes = new byte[1024 - 56];
-    writer.writeEvents(31, new UnsafeBuffer(bytes));
-
-    // when
-    final long lastBigEventPosition = writer.writeEvents(eventCount, BIG_EVENT_VALUE);
-
-    // then
-    assertThat(reader.seek(lastBigEventPosition)).isTrue();
-    final LoggedEvent bigEvent = reader.next();
-    assertThat(bigEvent.getKey()).isEqualTo(eventCount);
-    assertThat(bigEvent.getPosition()).isEqualTo(lastBigEventPosition);
-    assertThat(reader.hasNext()).isFalse();
-  }
-
-  @Test
-  public void shouldResizeBufferAndIterateOverSmallAndBigLoggedEvent() {
-    // given
-    final int eventCount = 500;
-    final long lastPosition = writer.writeEvents(eventCount, EVENT_VALUE);
-
-    // then
-    readerRule.assertEvents(eventCount - 1, EVENT_VALUE);
-    assertThat(reader.hasNext()).isTrue();
-
-    // when
-    final long bigEventPosition = writer.writeEvent(w -> w.key(eventKey).value(BIG_EVENT_VALUE));
-
-    // then
-    LoggedEvent loggedEvent = readerRule.nextEvent();
-    assertThat(loggedEvent.getKey()).isEqualTo(eventCount);
-    assertThat(loggedEvent.getPosition()).isEqualTo(lastPosition);
-
-    loggedEvent = readerRule.nextEvent();
-    assertThat(loggedEvent.getKey()).isEqualTo(eventKey);
-    assertThat(loggedEvent.getPosition()).isEqualTo(bigEventPosition);
-    assertThat(reader.hasNext()).isFalse();
-  }
-
-  @Test
-  public void shouldReturnBigLoggedEvent() {
-    // given
-    final long position = writer.writeEvent(w -> w.key(eventKey).value(BIG_EVENT_VALUE));
-
-    // then
-    final LoggedEvent loggedEvent = readerRule.nextEvent();
-    assertThat(loggedEvent.getKey()).isEqualTo(eventKey);
-    assertThat(loggedEvent.getPosition()).isEqualTo(position);
-    assertThat(reader.hasNext()).isFalse();
-  }
-
-  @Test
   public void shouldSeekToEnd() {
     // given
     final int eventCount = 1000;
-    final long lastPosition = writer.writeEvents(eventCount, BIG_EVENT_VALUE);
+    final long lastPosition = writer.writeEvents(eventCount, EVENT_VALUE);
 
     // when
     final long seekedPosition = reader.seekToEnd();
 
     // then
     assertThat(lastPosition).isEqualTo(seekedPosition);
-    assertThat(reader.hasNext()).isFalse();
-  }
-
-  @Test
-  public void shouldReturnBigLoggedEvents() {
-    // given
-    final int eventCount = 1000;
-
-    writer.writeEvents(eventCount, BIG_EVENT_VALUE);
-
-    // then
-    readerRule.assertEvents(eventCount, BIG_EVENT_VALUE);
     assertThat(reader.hasNext()).isFalse();
   }
 
@@ -361,6 +281,63 @@ public final class LogStreamReaderTest {
     final var result = reader.seekToEnd();
 
     // then
-    assertThat(result).isLessThan(0);
+    assertThat(result).isNegative();
+  }
+
+  @Test
+  public void shouldSeekToNextEventWhenThereIsNone() {
+    // given
+    final long lastEventPosition = writer.writeEvents(10, EVENT_VALUE);
+
+    // when
+    final boolean positionExists = reader.seekToNextEvent(lastEventPosition);
+
+    // then
+    assertThat(reader.hasNext()).isFalse();
+    assertThat(positionExists).isTrue();
+    assertThat(reader.getPosition()).isEqualTo(lastEventPosition);
+  }
+
+  @Test
+  public void shouldSeekToNextEvent() {
+    // given
+    final long lastEventPosition = writer.writeEvents(10, EVENT_VALUE);
+
+    // when
+    final boolean positionExists = reader.seekToNextEvent(lastEventPosition - 1);
+
+    // then
+    assertThat(positionExists).isTrue();
+    assertThat(reader).hasNext();
+    assertThat(reader.next().getPosition()).isEqualTo(lastEventPosition);
+  }
+
+  @Test
+  public void shouldNotSeekToNextEvent() {
+    // given
+    final long lastEventPosition = writer.writeEvents(10, EVENT_VALUE);
+
+    // when
+    final boolean positionExists = reader.seekToNextEvent(lastEventPosition + 1);
+
+    // then
+    assertThat(positionExists).isFalse();
+    assertThat(reader.hasNext()).isFalse();
+  }
+
+  @Test
+  public void shouldSeekToFirstEventWhenNextIsNegative() {
+    // given
+    final long firstEventPosition = writer.writeEvent(EVENT_VALUE);
+    writer.writeEvents(10, EVENT_VALUE);
+    reader.seekToEnd();
+
+    // when
+    final boolean positionExists = reader.seekToNextEvent(-1);
+
+    // then
+    assertThat(positionExists).isTrue();
+    assertThat(reader).hasNext();
+    assertThat(reader.next().getPosition()).isEqualTo(firstEventPosition);
   }
 }
